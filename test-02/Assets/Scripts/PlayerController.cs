da50610fa38df14c19f2379ed1174cb023f46545
kalineh <kalin.eh@gmail.com>
2015-02-11 16:12:48 -0800

    unity: test-02 tutorial ball

---
+++ test-02/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	[Tooltip("Movement speed")]
+	[Range(5.0f, 25.0f)]
+	public float Speed = 5.0f;
+
+	// called before rendering a frame, where most of game code will go
+	void Update()
+	{
+
+	}
+
+	// called just before physics update
+	void FixedUpdate()
+	{
+		float moveh = Input.GetAxis("Horizontal");
+		float movev = Input.GetAxis("Vertical");
+
+		var force = 
+			Vector3.forward * movev +
+			Vector3.right * moveh;
+
+		rigidbody.AddForce(force * this.Speed);
+
+	}
+}
