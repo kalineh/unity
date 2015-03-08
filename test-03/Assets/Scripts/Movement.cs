bb4e4039f0a1db69c0a35a06451432afe4cea593
kalineh <kalin.eh@gmail.com>
2015-03-08 15:12:39 -0700

    unity: test3 spam

---
+++ test-03/Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+//using System.Collections;
+
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		const float Scale = 50.0f;
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+
+		var force =
+			Vector3.right * x * Scale +
+			Vector3.forward * y * Scale;
+
+		var t = Time.deltaTime;
+		var rb = GetComponent<Rigidbody>();
+
+		rb.AddForce(force * t);
+	}
+}
