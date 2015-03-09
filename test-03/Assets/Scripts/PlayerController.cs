e72919701e033b9e2fd02aee88cffe9eb807a9b0
kalin.eh <kalin.eh@gmail.com>
2015-03-08 18:01:06 -0700

    unity: test3 spam

---
+++ test-03/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+//using System.Collections;
+
+
+public class PlayerController : MonoBehaviour
+{
+	private int count;
+	public Text scoreText;
+
+	void Start()
+	{
+		count = 0;
+		UpdateScore();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		const float Scale = 250.0f;
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+
+		var force =
+			Vector3.right * x * Scale +
+			Vector3.forward * y * Scale;
+
+		var t = Time.deltaTime;
+		var rb = GetComponent<Rigidbody>();
+
+		rb.AddForce(force * t);
+	}
+
+	void OnTriggerEnter(Collider rhs)
+	{
+		if (rhs.tag == "Pickup")
+		{
+			rhs.gameObject.SetActive(false);
+
+			count++;
+			UpdateScore();
+		}
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = string.Format("Score: {0}", count);
+	}
+}
---
+++ test-03/Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+	public float Speed = 1.0f;
+
+	void Update()
+	{
+		float x = Speed;
+		float y = Speed;
+		float z = Speed;
+
+		transform.Rotate(x, y, z);
+	}
+}
