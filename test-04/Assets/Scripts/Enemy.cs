671c20118d50cec8201f73196d0bfa956fc91cdb
kalin.eh <kalin.eh@gmail.com>
2015-03-12 00:00:12 -0700

    unity: roguelike wip

---
+++ test-04/Assets/Scripts/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MovingObject
+{
+	public int playerDamage;
+
+	private Animator animator;
+	private Transform target;
+
+	private bool skipMove;
+
+	// Use this for initialization
+	protected override void Start ()
+	{
+		GameManager.instance.AddEnemyToList(this);
+
+		animator = GetComponent<Animator>();
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	protected override void AttemptMove<T>(int xd, int yd)
+	{
+		if (skipMove)
+		{
+			skipMove = false;
+			return;
+		}
+
+		base.AttemptMove<T>(xd, yd);
+
+		skipMove = true;
+	}
+
+	public void MoveEnemy()
+	{
+		var ofs = target.position - transform.position;
+
+		int xd = (int)Mathf.Sign(ofs.x);
+		int yd = (int)Mathf.Sign(ofs.y);
+
+		if (xd != 0)
+			yd = 0;
+
+		AttemptMove<Player>(xd, yd);
+	}
+
+	protected override void OnCantMove<T>(T component)
+	{
+		Player hitPlayer = component as Player;
+
+		animator.SetTrigger("enemyAttack");
+
+		hitPlayer.LoseFood(playerDamage);
+	}
+}
+
+
+
+
+
+
+
+
--- test-04/Assets/Scripts/GameManager.cs
+++ test-04/Assets/Scripts/GameManager.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+	public float turnDelay = 0.2f;
 	public static GameManager instance = null;
 	public BoardManager boardScript;
 
 	private int level = 3;
 
 	public int playerFoodPoints = 100;
+
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
 
 	[HideInInspector]
 	public bool playersTurn = true;
@@ -22,6 +27,7 @@ public class GameManager : MonoBehaviour {
 
 		DontDestroyOnLoad(gameObject);
 
+		enemies = new List<Enemy>();
 		boardScript = GetComponent<BoardManager>();
 		InitGame();
 
@@ -29,6 +35,7 @@ public class GameManager : MonoBehaviour {
 
 	void InitGame()
 	{
+		enemies.Clear();
 		boardScript.SetupScene(level);
 	}
 
@@ -44,6 +51,43 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (playersTurn || enemiesMoving)
+			return;
+
+		StartCoroutine(MoveEnemies());
+	}
+
+	public void AddEnemyToList(Enemy script)
+	{
+		enemies.Add(script);
+	}
+
+	IEnumerator MoveEnemies()
+	{
+		enemiesMoving = true;
+		yield return new WaitForSeconds(turnDelay);
+
+		if (enemies.Count == 0)
+		{
+			yield return new WaitForSeconds(turnDelay);
+		}
+
+		for (int i = 0; i < enemies.Count; ++i)
+		{
+			enemies[i].MoveEnemy();
+			yield return new WaitForSeconds(enemies[i].moveTime);
+		}
+
+		playersTurn = true;
+		enemiesMoving = false;
 	}
 }
+
+
+
+
+
+
+
+
+
